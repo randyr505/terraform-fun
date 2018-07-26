c276a1d7d5f0ac64f4f1f5e95c1d81af971a9b75
Randy L Rasmussen <randyr505@gmail.com>
2018-07-26 18:52:43 -0500

    updated slack url

--- scripts/notify_slack.sh
+++ scripts/notify_slack.sh
@@ -2,6 +2,6 @@
 
 message=$(echo "$*" | sed 's/"/\"/g' | sed "s/'/\'/g" )
 json="{\"text\": \"$message\"}"
-url="https://hooks.slack.com/services/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"
+url="https://hooks.slack.com/services/AAA/AAA/AAA"
 
 curl -X POST -H 'Content-type: application/json' -d "$json" $url
